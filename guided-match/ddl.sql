2e15c89ebe97ce99c775f1d847a91dfac42e8a62
Trevor <59959500+trevorcummingsCCS@users.noreply.github.com>
2020-06-02 09:20:13 +0100

    Removed commented blocks for Commercial Agreements & Lots

--- guided-match/ddl.sql
+++ guided-match/ddl.sql
@@ -25,23 +25,6 @@ CREATE TABLE ccs_outcomes (
 );
 */
 
-/* This table creation script belongs in the agreement ddl.sql
-CREATE TABLE commercial_agreements (
-  commercial_agreement_id           INTEGER PRIMARY KEY,
-  commercial_agreement_number       VARCHAR(20) NOT NULL,
-  commercial_agreement_name         VARCHAR(200) NOT NULL,
-  commercial_agreement_owner        VARCHAR(200) NOT NULL,
-  description                       VARCHAR(2000) NOT NULL, -- perhaps this needs to be a different type
-  start_date                        DATE NOT NULL,
-  end_date                          DATE NOT NULL,
-  agreement_url                     VARCHAR(2000) NOT NULL,
-  authorisation_email               VARCHAR(200)
-);
-CREATE INDEX COAG_IDX1 on COMMERCIAL_AGREEMENTS  (commercial_agreement_number);
-CREATE INDEX COAG_IDX2 on COMMERCIAL_AGREEMENTSS (commercial_agreement_name);
-CREATE INDEX COAG_IDX3 on COMMERCIAL_AGREEMENTS  (commercial_agreement_owner);
-CREATE INDEX COAG_IDX4 on COMMERCIAL_AGREEMENTS  (authorisation_email);
-*/
 
 /* Neo4J 
 CREATE TABLE domain_question_outcomes (
@@ -130,20 +113,7 @@ CREATE TABLE lot_outcomes (
 );
 */
 
-/* Similar to the commercial_agreements table this one too also belongs in the agreements service
-CREATE TABLE lots (
-  lot_id                            INTEGER PRIMARY KEY,
-  lot_number                        VARCHAR(20) NOT NULL,
-  lot_name                          VARCHAR(200) NOT NULL,
-  commercial_agreement_id           INTEGER NOT NULL,
-  lot_type                          VARCHAR(25) NOT NULL, -- Valid values Services, Products or Products and Services
-  start_date                        DATE NOT NULL,
-  end_date                          DATE NOT NULL
-);
-CREATE INDEX LOTS_IDX1 on LOTS(lot_number);
-CREATE INDEX LOTS_IDX2 on LOTS(lot_name);
-CREATE INDEX LOTS_IDX3 on LOTS(commercial_agreement_id);
-*/
+
 
 /* Neo4J
 CREATE TABLE question_definitions (
