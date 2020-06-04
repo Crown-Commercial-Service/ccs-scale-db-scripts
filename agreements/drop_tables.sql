4ec267a22845b77ca5cc7e633d0225e98b0487de
Trevor <59959500+trevorcummingsCCS@users.noreply.github.com>
2020-06-04 09:47:44 +0100

    File to drop tables for teh Agreements service

---
+++ agreements/drop_tables.sql
@@ -0,0 +1,18 @@
+/*
+Title       Drop table script for Agreement Service
+
+Date        June 4th 2020
+
+Author      Trevor Cummings
+
+Description This file is a script to drop the tables for the Agreements database. 
+
+*/
+
+DROP TABLE lot_sectors;
+
+DROP TABLE sectors;
+
+DROP TABLE lots;
+
+DROP TABLE commercial_agreements;
