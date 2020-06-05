1db35297ec7f95cae6cdd0c4cde5ca629b995726
tommyb82 <thomasbunting@gmail.com>
2020-06-05 14:15:17 +0100

    Remove commented out tables from GM that are managed by Neo4J

--- guided-match/ddl.sql
+++ guided-match/ddl.sql
@@ -1,61 +1,10 @@
 /*
 Title       Database creation script for FaT database design
 
-Date        May 11th 2020
-
-Author      Trevor Cummings
-
 Description This file is a script to create the tables for the Find a Thing database. The data model for FaT will be deployed in
             two separate databases. One will be a postgreSQL database for our relational tables and the other will be a Neo4j
             for the graph database.
 
-            I will comment out the tables which are for the graph database as the create table statements will be created
-            in a separate script
-
-
-*/
-
-/* Neo4J
-CREATE TABLE ccs_outcomes (
-   outcome_id                       INTEGER PRIMARY KEY,
-   journey_question_id              INTEGER NOT NULL,
-   outcome_evaluator_code           VARCHAR(10) NOT NULL,
-   journey_answer_id                INTEGER NOT NULL,
-   ccs_support_ind                  CHAR(1) NOT NULL
-);
-*/
-
-
-/* Neo4J
-CREATE TABLE domain_question_outcomes (
-   outcome_id                       INTEGER PRIMARY KEY,
-   journey_question_id              INTEGER NOT NULL,
-   outcome_evaluator_code           VARCHAR(10) NOT NULL,
-   journey_answer_id                INTEGER NOT NULL,
-   modifier_journey_name            VARCHAR(10) NOT NULL
-);
-*/
-/* Neo4J
-CREATE TABLE explicit_question_outcomes (
-   outcome_id                       INTEGER PRIMARY KEY,
-   journey_question_id              INTEGER NOT NULL,
-   outcome_evaluator_code           VARCHAR(10) NOT NULL,
-   journey_answer_id                INTEGER NOT NULL,
-   resulting_journey_question_id    INTEGER NOT NULL
-);
-*/
-
-/* Neo4J
-CREATE TABLE journey_answers ( --Need to check this one out with Dave H as attributes on model are different to descriptions on confluence page
-  journey_answer_id                INTEGER PRIMARY KEY,
-  journey_question_id              INTEGER NOT NULL,
-  answer_type                      VARCHAR(50),
-  answer_label                     VARCHAR(100),
-  default_ind                      CHAR(1), -- Y or N default value will be N as only one can be Y
-  valid_value                      VARCHAR(200),
-  url                              VARCHAR(200), -- notsure required anymore
-  regex_validation                 VARCHAR(2000)  -- notsure required anymore
-);
 */
 
 CREATE TABLE journey_instance_answers (
@@ -88,22 +37,6 @@ CREATE TABLE journey_instances (
 );
 CREATE INDEX JOIN_IDX1 ON JOURNEY_INSTANCES(journey_start_date);
 
-/* Neo4J
-CREATE TABLE journey_questions (
-  journey_question_id               INTEGER PRIMARY KEY,
-  question_id                       INTEGER NOT NULL,
-  question_order                    SMALLINT NOT NULL ,
-  regex_validation                  VARCHAR(2000),
-  min_threshold_range               NUMBER(12,2),
-  max_threshold_range               NUMBER(12,2),
-  threshold_value                   NUMBER(12,2),
-  min_threshold_date                DATE,
-  max_Threshold_date                DATE,
-  threshhold_date                   DATE
-);
-
-*/
-
 /* This table could exist in some form in both databases */
 CREATE TABLE journeys (
   journey_id                        UUID PRIMARY KEY,
@@ -113,49 +46,6 @@ CREATE TABLE journeys (
 );
 CREATE INDEX JOUR_IDX1 on JOURNEYS(journey_name);
 CREATE INDEX JOUR_IDX2 on JOURNEYS(parent_journey_id);
-
-/* Neo4J
-CREATE TABLE lot_outcomes (
-   outcome_id                       INTEGER PRIMARY KEY,
-   journey_question_id              INTEGER NOT NULL,
-   outcome_evaluator_code           VARCHAR(10) NOT NULL,
-   journey_answer_id                INTEGER NOT NULL,
-   lot_number                       VARCHAR(10) NOT NULL
-);
-*/
-
-
-
-/* Neo4J
-CREATE TABLE question_definitions (
-  question_id                       INTEGER PRIMARY KEY,
-  question_type                     VARCHAR(20) NOT NULL,
-  question_description              VARCHAR(2000) NOT NULL,
-  question_text                     VARCHAR(200) NOT NULL,
-  published_status                  CHAR(1) NOT NULL, -- Y(es) or N(o),
-  validate_via_agreement            CHAR(1) NOT NULL, -- Y(es) or N(o),
-);
-*/
-/* Neo4J
-CREATE TABLE question_groups (
-  question_group_id                 INTEGER PRIMARY KEY,
-  question_group_name               VARCHAR(100) NOT NULL,
-  published_status                  CHAR(1) NOT NULL -- Need to check if we don't want to make this boolean
-);
-*/
-/* Neo4J
-CREATE TABLE question_group_members (
-  question_group_member_id          INTEGER PRIMARY KEY,
-  question_group_name               VARCHAR(200) NOT NULL,
-  published_status                  CHAR(1) NOT NULL-- Y(es) or N(o),
-);
-*/
-/* Neo4J
-CREATE TABLE question_outcome_evaluators (
-  outcome_evaluator_code            VARCHAR(10) PRIMARY KEY,
-  outcome_evaluator_description     VARCHAR(200) NOT NULL
-);
-*/
 
 CREATE TABLE search_domains (
   domain_modifier_id               SERIAL PRIMARY KEY,
@@ -176,17 +66,7 @@ CREATE TABLE search_terms (
 
 CREATE INDEX SETE_IDX1 ON SEARCH_TERMS (search_term);
 
-/* Neo4J
-CREATE TABLE standard_answers  (
-  standard_answer_id                INTEGER PRIMARY KEY,
-  valid_value                       VARCHAR(200),
-  answer_label                      VARCHAR(100),
-);
-*/
-
 /* Referential constraints */
-
-
 ALTER TABLE journey_instance_questions
 ADD CONSTRAINT journey_instance_questions_journey_instances_fk FOREIGN KEY (journey_instance_id)
     REFERENCES journey_instances (journey_instance_id);
