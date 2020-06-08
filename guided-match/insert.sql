e98bc8ef02bd53ace0f835d937033d9abd3e89f8
tommyb82 <thomasbunting@gmail.com>
2020-06-08 13:46:21 +0100

    Fix L&L journey ID

--- guided-match/insert.sql
+++ guided-match/insert.sql
@@ -7,7 +7,7 @@ DELETE FROM search_terms;
 DELETE FROM journeys;
 
 INSERT INTO journeys (journey_id, journey_name, published)
-VALUES ('ccb5a43a-75b5-11ea-bc55-0242ac130003', 'Linen & Laundry', true),
+VALUES ('b87a0636-654e-11ea-bc55-0242ac130003', 'Linen & Laundry', true),
 ('ccb5c730-75b5-11ea-bc55-0242ac130003', 'Wider Public Sector Legal Services', true),
 ('ccb6174e-75b5-11ea-bc55-0242ac130003', 'Tech & EduTech', true);
 
@@ -15,12 +15,12 @@ INSERT INTO "search_terms" (search_term)
 VALUES ('linen'),('laundry services'),('legal'),('laptops');
 
 INSERT INTO search_domains (search_id, journey_id)
-SELECT search_id, 'ccb5a43a-75b5-11ea-bc55-0242ac130003'
+SELECT search_id, 'b87a0636-654e-11ea-bc55-0242ac130003'
 FROM search_terms
 WHERE search_term = 'linen';
 
 INSERT INTO search_domains (search_id, journey_id)
-SELECT search_id, 'ccb5a43a-75b5-11ea-bc55-0242ac130003'
+SELECT search_id, 'b87a0636-654e-11ea-bc55-0242ac130003'
 FROM search_terms
 WHERE search_term = 'laundry services';
 
