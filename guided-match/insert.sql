456fda742e9e331c133361e213847b13db474e44
tommyb82 <thomasbunting@gmail.com>
2020-06-09 15:36:20 +0100

    GM search_domain new col values

--- guided-match/insert.sql
+++ guided-match/insert.sql
@@ -14,22 +14,22 @@ VALUES ('b87a0636-654e-11ea-bc55-0242ac130003', 'Linen & Laundry', true),
 INSERT INTO "search_terms" (search_term)
 VALUES ('linen'),('laundry services'),('legal'),('laptops');
 
-INSERT INTO search_domains (search_id, journey_id)
-SELECT search_id, 'b87a0636-654e-11ea-bc55-0242ac130003'
+INSERT INTO search_domains (search_id, journey_id, modifier_journey_name, journey_selection_text, journey_selection_description)
+SELECT search_id, 'b87a0636-654e-11ea-bc55-0242ac130003', 'hospitals', 'L&L hospital journey', 'Linen & Laundry hospital specific GM journey'
 FROM search_terms
 WHERE search_term = 'linen';
 
-INSERT INTO search_domains (search_id, journey_id)
-SELECT search_id, 'b87a0636-654e-11ea-bc55-0242ac130003'
+INSERT INTO search_domains (search_id, journey_id, modifier_journey_name, journey_selection_text, journey_selection_description)
+SELECT search_id, 'b87a0636-654e-11ea-bc55-0242ac130003', 'bedsheets', 'L&L bedsheets journey', 'Linen & Laundry bedsheets specific GM journey'
 FROM search_terms
 WHERE search_term = 'laundry services';
 
-INSERT INTO search_domains (search_id, journey_id)
-SELECT search_id, 'ccb5c730-75b5-11ea-bc55-0242ac130003'
+INSERT INTO search_domains (search_id, journey_id, modifier_journey_name, journey_selection_text, journey_selection_description)
+SELECT search_id, 'ccb5c730-75b5-11ea-bc55-0242ac130003', 'contracts', 'Legal Services contracts journey', 'WPS Legal Services contracts specific journey'
 FROM search_terms
 WHERE search_term = 'legal';
 
-INSERT INTO search_domains (search_id, journey_id)
-SELECT search_id, 'ccb6174e-75b5-11ea-bc55-0242ac130003'
+INSERT INTO search_domains (search_id, journey_id, modifier_journey_name, journey_selection_text, journey_selection_description)
+SELECT search_id, 'ccb6174e-75b5-11ea-bc55-0242ac130003', 'conveyancing', 'Legal Services conveyancing journey', 'WPS Legal Services conveyancing specific journey'
 FROM search_terms
 WHERE search_term = 'laptops';
