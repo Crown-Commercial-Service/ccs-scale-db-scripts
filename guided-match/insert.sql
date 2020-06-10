5e6988054cef30f6a0dfc72d64c309572159a1b3
tommyb82 <thomasbunting@gmail.com>
2020-06-10 13:09:27 +0100

    Insert explicit UUIDs in sample search_domain data

--- guided-match/insert.sql
+++ guided-match/insert.sql
@@ -14,22 +14,22 @@ VALUES ('b87a0636-654e-11ea-bc55-0242ac130003', 'Linen & Laundry', true),
 INSERT INTO "search_terms" (search_term)
 VALUES ('linen'),('laundry services'),('legal'),('laptops');
 
-INSERT INTO search_domains (search_id, journey_id, modifier_journey_name, journey_selection_text, journey_selection_description)
-SELECT search_id, 'b87a0636-654e-11ea-bc55-0242ac130003', 'hospitals', 'L&L hospital journey', 'Linen & Laundry hospital specific GM journey'
+INSERT INTO search_domains (search_id, journey_id, modifier_journey_name, journey_selection_text, journey_selection_description, lookup_entry_id)
+SELECT search_id, 'b87a0636-654e-11ea-bc55-0242ac130003', 'hospitals', 'L&L hospital journey', 'Linen & Laundry hospital specific GM journey', 'a3a9783f-3ccd-4bb9-ae9b-63d6ea62e440'
 FROM search_terms
 WHERE search_term = 'linen';
 
-INSERT INTO search_domains (search_id, journey_id, modifier_journey_name, journey_selection_text, journey_selection_description)
-SELECT search_id, 'b87a0636-654e-11ea-bc55-0242ac130003', 'bedsheets', 'L&L bedsheets journey', 'Linen & Laundry bedsheets specific GM journey'
+INSERT INTO search_domains (search_id, journey_id, modifier_journey_name, journey_selection_text, journey_selection_description, lookup_entry_id)
+SELECT search_id, 'b87a0636-654e-11ea-bc55-0242ac130003', 'bedsheets', 'L&L bedsheets journey', 'Linen & Laundry bedsheets specific GM journey', 'ef55cae4-df47-466a-aef1-45a5d2008e02'
 FROM search_terms
 WHERE search_term = 'laundry services';
 
-INSERT INTO search_domains (search_id, journey_id, modifier_journey_name, journey_selection_text, journey_selection_description)
-SELECT search_id, 'ccb5c730-75b5-11ea-bc55-0242ac130003', 'contracts', 'Legal Services contracts journey', 'WPS Legal Services contracts specific journey'
+INSERT INTO search_domains (search_id, journey_id, modifier_journey_name, journey_selection_text, journey_selection_description, lookup_entry_id)
+SELECT search_id, 'ccb5c730-75b5-11ea-bc55-0242ac130003', 'contracts', 'Legal Services contracts journey', 'WPS Legal Services contracts specific journey', 'e667b2b5-ab4d-4949-ac35-2eb4e5754992'
 FROM search_terms
 WHERE search_term = 'legal';
 
-INSERT INTO search_domains (search_id, journey_id, modifier_journey_name, journey_selection_text, journey_selection_description)
-SELECT search_id, 'ccb6174e-75b5-11ea-bc55-0242ac130003', 'conveyancing', 'Legal Services conveyancing journey', 'WPS Legal Services conveyancing specific journey'
+INSERT INTO search_domains (search_id, journey_id, modifier_journey_name, journey_selection_text, journey_selection_description, lookup_entry_id)
+SELECT search_id, 'ccb6174e-75b5-11ea-bc55-0242ac130003', 'conveyancing', 'Legal Services conveyancing journey', 'WPS Legal Services conveyancing specific journey', 'c3360661-f3b5-4c78-bf37-a7f32bfb449f'
 FROM search_terms
 WHERE search_term = 'laptops';
