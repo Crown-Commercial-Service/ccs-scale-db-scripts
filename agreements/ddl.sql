2c30c744914adedb56c4653653ffef669435c441
Trevor <59959500+trevorcummingsCCS@users.noreply.github.com>
2020-06-15 17:15:14 +0100

    Added route_to_market and lot_route_to_market (#8)
    * Added route_to_market and lot_route_to_market
    
    SQL Fiddle had  trouble with a primary key against two columns but as postgres  lot_sectors coped with it I am thinking that lot_route_to_market will cope too.
    
    * Amended Primary Keys and renamed columns
    
    Added the correct definition for composite primary key columns.
    Renamed commecial_agreements.description to commercial_agreements.commercial_agreement_description
    Put the uom unit_of_measure columns back as they were defined in the api
    
    * Added the two new tables to the drop file
    
    * Amended sector_description from sector_descripton
    
    * Removed the quantity columns from lot_route_to_market
    
    * Removed column.
    
    Removed authorisation email as the model has been changed so that you could have multiple authorisation email addresses as other types as well.

--- agreements/ddl.sql
+++ agreements/ddl.sql
@@ -13,11 +13,10 @@ CREATE TABLE commercial_agreements (
   commercial_agreement_number       VARCHAR(20) NOT NULL,
   commercial_agreement_name         VARCHAR(200) NOT NULL,
   commercial_agreement_owner        VARCHAR(200) NOT NULL,
-  description                       VARCHAR(2000) NOT NULL, -- perhaps this needs to be a different type
+  commercial_agreement_description  VARCHAR(2000) NOT NULL, -- perhaps this needs to be a different type
   start_date                        DATE NOT NULL,
   end_date                          DATE NOT NULL,
-  agreement_url                     VARCHAR(2000) NOT NULL,
-  authorisation_email               VARCHAR(200)
+  agreement_url                     VARCHAR(2000) NOT NULL
 );
 
 CREATE INDEX COAG_IDX1 on COMMERCIAL_AGREEMENTS  (commercial_agreement_number);
@@ -43,7 +42,7 @@ CREATE INDEX LOTS_IDX3 on LOTS(commercial_agreement_id);
 CREATE TABLE sectors (
   sector_code                       VARCHAR(2) PRIMARY KEY,
   sector_name                       VARCHAR(20) NOT NULL,
-  sector_descripton                 VARCHAR(2000) NOT NULL
+  sector_description                 VARCHAR(2000) NOT NULL
 );
 
 CREATE INDEX SECTORS_IDX1 on SECTORS(sector_code);
@@ -51,11 +50,33 @@ CREATE INDEX SECTORS_IDX2 on SECTORS(sector_name);
 
 
 CREATE TABLE lot_sectors (
-  lot_id                            INTEGER PRIMARY KEY,
-  sector_code                       VARCHAR(2) PRIMARY KEY
+  lot_id                            INTEGER ,
+  sector_code                       VARCHAR(2) ,
+  PRIMARY KEY (lot_id,sector_code)          
 );
 
 CREATE INDEX LOT_SECTORS_IDX1 on LOT_SECTORS (lot_id,sector_code);
+
+CREATE TABLE route_to_market (
+  route_to_market_name              VARCHAR(50) PRIMARY KEY,
+  route_to_market_description       VARCHAR(2000) NOT NULL
+);
+
+CREATE TABLE lot_route_to_market (
+  lot_id                            INTEGER ,
+  route_to_market_name              VARCHAR(50), 
+  start_date                        DATE NOT NULL,
+  end_date                          DATE NOT NULL,
+  location                          VARCHAR(20), -- For example, Regional or National  
+  buying_method_url                 VARCHAR(2000),
+  lot_minimum_value                 NUMERIC(18,4),
+  lot_maximum_value                 NUMERIC(18,4),   
+  lot_contract_length_uom           VARCHAR(10),          
+  lot_contract_length_minimum_value SMALLINT, 
+  lot_contract_length_maximum_value SMALLINT,
+  PRIMARY KEY (lot_id,route_to_market_name)          
+);
+         
 
 ALTER TABLE lots 
 ADD CONSTRAINT lots_commercial_agreement_fk FOREIGN KEY (commercial_agreement_id) 
@@ -69,3 +90,10 @@ ALTER TABLE lot_sectors
 ADD CONSTRAINT lot_sectors_sectors_fk FOREIGN KEY (sector_code) 
     REFERENCES sectors (sector_code);        
 
+ALTER TABLE lot_route_to_market
+ADD CONSTRAINT lot_route_to_market_lots_fk FOREIGN KEY (lot_id) 
+    REFERENCES lots (lot_id);        
+
+ALTER TABLE lot_route_to_market
+ADD CONSTRAINT lot_route_to_market_route_to_market_fk FOREIGN KEY (route_to_market_name) 
+    REFERENCES route_to_market (route_to_market_name);     
