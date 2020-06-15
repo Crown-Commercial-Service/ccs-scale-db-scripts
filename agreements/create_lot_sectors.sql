91bfa2a35c2a1108f2936a933fb49700c8dbf0c8
Trevor <59959500+trevorcummingsCCS@users.noreply.github.com>
2020-06-15 19:01:10 +0100

    Just setting files up to edit later

---
+++ agreements/create_lot_sectors.sql
@@ -0,0 +1,14 @@
+https://github.com/Crown-Commercial-Service/ccs-scale-db-scripts/*
+Title       create_lot_sectors.sql
+Date        June 15th 2020
+Author      Trevor Cummings
+Description This file is a script to create test data for lot_sectors         
+*/
+
+INSERT INTO LOT_SECTORS
+(lot_id,
+ sector_code)
+values
+(1,
+ 'CG')
+; 
