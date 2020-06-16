7a47f8cdcd38cbe583a0bd8a35855dc71c90596b
Trevor <59959500+trevorcummingsCCS@users.noreply.github.com>
2020-06-16 12:07:39 +0100

    Committing the changes for RM6103

--- agreements/create_lots.sql
+++ agreements/create_lots.sql
@@ -189,7 +189,7 @@ values
  start_date,
  end_date)
 values 
-(4,
+(10,
  'Lot 4',
  'Information Assured Products & Associated Services',
  'Information Assured Products & Associated Services description',
@@ -198,4 +198,99 @@ values
  '10-Dec-2019',
  '09-Dec-2021');
  
+ -- RM 6103 CA FK 3, 5 Lots
  
+ INSERT INTO lOTS
+(lot_id,
+ lot_number,
+ lot_name,
+ lot_description,          
+ commercial_agreement_id, -- FK back to commercial agreements
+ lot_type,                -- Valid values Services, Products or Products and Services
+ start_date,
+ end_date)
+values 
+(11,
+ 'Lot 1',
+ 'ICT Solutions',
+ 'This is a direct replacement of ICT Services for Education (RM1050) agreement. The agreement provides customers with the capability to obtain ICT goods and or services for their specific needs, whether it is a new build requirement or an ICT refresh.',
+ 3,
+ 'Services',
+ '10-Jun-2019',
+ '10-Jun-2022');
+ 
+ INSERT INTO lOTS
+(lot_id,
+ lot_number,
+ lot_name,
+ lot_description,          
+ commercial_agreement_id, -- FK back to commercial agreements
+ lot_type,                -- Valid values Services, Products or Products and Services
+ start_date,
+ end_date)
+values 
+(12,
+ 'Lot 2',
+ 'Broadband Fibre Infrastructure',
+ 'The provision of a design and installation of broadband fibre infrastructure. Suppliers under this lot are capable of supplying overall design, provision of fibre to the premises, testing of fibre and ''last mile'' installation.',
+ 3,
+ 'Services',
+ '10-Jun-2019',
+ '10-Jun-2022');
+ 
+ INSERT INTO lOTS
+(lot_id,
+ lot_number,
+ lot_name,
+ lot_description,          
+ commercial_agreement_id, -- FK back to commercial agreements
+ lot_type,                -- Valid values Services, Products or Products and Services
+ start_date,
+ end_date)
+values 
+(13,
+ 'Lot 3',
+ 'Broadband Services',
+ 'The provision of connectivity services. Suppliers under this lot are capable of supplying full fibre broadband services (new services/upgrade of existing), support and maintenance, internet filtering, firewall, eSafety and IP connectivity.',
+ 3,
+ 'Services',
+ '10-Jun-2019',
+ '10-Jun-2022');
+ 
+ INSERT INTO lOTS
+(lot_id,
+ lot_number,
+ lot_name,
+ lot_description,          
+ commercial_agreement_id, -- FK back to commercial agreements
+ lot_type,                -- Valid values Services, Products or Products and Services
+ start_date,
+ end_date)
+values 
+(14,
+ 'Lot 4',
+ 'Hardware',
+ 'The provision of ICT hardware, software and some networking equipment. Suppliers can supply hardware (laptops, desktops), networking: (switches, cabling, routers), peripherals (printers, mice, charging trolleys) and software (part of a package).',
+ 3,
+ 'Services',
+ '10-Jun-2019',
+ '10-Jun-2022');
+ 
+ INSERT INTO lOTS
+(lot_id,
+ lot_number,
+ lot_name,
+ lot_description,          
+ commercial_agreement_id, -- FK back to commercial agreements
+ lot_type,                -- Valid values Services, Products or Products and Services
+ start_date,
+ end_date)
+values 
+(15,
+ 'Lot 5',
+ 'Audio Visual',
+ 'The provision and installation of sound and/or visual equipment. Suppliers under this lot are capable of supplying interactive solutions (whiteboards, tables), digital signage, televisions, AV software, auditorium solutions, training.',
+ 3,
+ 'Services',
+ '10-Jun-2019',
+ '10-Jun-2022');
