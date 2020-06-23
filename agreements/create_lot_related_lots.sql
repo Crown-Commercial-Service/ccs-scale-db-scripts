1667435a29583d6f201a0dfc75f11a0e175491ee
Trevor <59959500+trevorcummingsCCS@users.noreply.github.com>
2020-06-23 17:23:56 +0100

    Create create_lot_related_lots.sql

---
+++ agreements/create_lot_related_lots.sql
@@ -0,0 +1,6 @@
+/*
+Title       create_lot_related_lots.sql
+Date        June 23rd 2020
+Author      Trevor Cummings
+Description This file is a script to create data for lot related lots        
+*/
