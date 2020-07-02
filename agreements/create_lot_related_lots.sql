1b5577c35e15674ad7f1687c5aff61bd2399e289
Trevor <59959500+trevorcummingsCCS@users.noreply.github.com>
2020-07-02 14:13:17 +0100

    Lot id and lot rule id were round the wrong way (#18)

--- agreements/create_lot_related_lots.sql
+++ agreements/create_lot_related_lots.sql
@@ -5,4 +5,4 @@ Author      Trevor Cummings
 Description This file is a script to create data for lot related lots        
 */
 INSERT INTO LOT_RELATED_LOTS (lot_id, lot_rule_id, relationship_description)
-VALUES (6,10,'FurtherCompetitionWhenBudgetExceeded');
+VALUES (10,6,'FurtherCompetitionWhenBudgetExceeded');
