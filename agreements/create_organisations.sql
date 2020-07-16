4727ef99c6ae5894068d9252af8994ca5ee77cf2
Trevor <59959500+trevorcummingsCCS@users.noreply.github.com>
2020-07-16 10:44:55 +0100

    Create create_organisations.sql

---
+++ agreements/create_organisations.sql
@@ -0,0 +1,31 @@
+Title       create_organisations.sql
+Date        July 16th 2020
+Author      Trevor Cummings
+Description This file is a script to create data for ORGANISATIONS. This data eventually will reside in Conclave     
+*/
+INSERT INTO ORGANISATIONS (
+  organisation_name,
+  alias_name,
+  supplier_ind,
+  buyer_ind,
+  business_type,
+  organisation_uri,
+  creation_date,
+  country_of_creation,
+  parent_org_id,
+  top_level_org_id,
+  organisation_status)
+  values 
+('ACS Business Supplies Ltd',
+ 'ACSACS',
+ 1,
+ 0,
+ 1,
+ 'www.acsacs.co.uk',
+ '01-Jan-2018',
+ 'GB',
+ null,
+ null,
+ 'ACTIVE',
+ 1);
+        
