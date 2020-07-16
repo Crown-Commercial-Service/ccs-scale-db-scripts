557af6f5a12e0e81e9ee407fa171f3c0ee05f5e9
Trevor <59959500+trevorcummingsCCS@users.noreply.github.com>
2020-07-16 21:32:24 +0100

    Updated tdue to conclave designchnage

--- agreements/create_organisations.sql
+++ agreements/create_organisations.sql
@@ -6,21 +6,15 @@ Description This file is a script to create data for ORGANISATIONS. This data ev
 */
 INSERT INTO ORGANISATIONS (
   organisation_name,
-  alias_name,
-  supplier_ind,
-  buyer_ind,
   business_type,
   organisation_uri,
-  creation_date,
-  country_of_creation,
+  incorporation_date,
+  country_of_incorporation,
   parent_org_id,
-  top_level_org_id,
+  ultimate_org_id,
   organisation_status)
   values 
 ('ACS Business Supplies Ltd',
- 'ACSACS',
- B'1',
- B'0',
  1,
  'www.acsacs.co.uk',
  '01-Jan-2018',
