48be51afaf79da4ea1ab04ad5c85f7c34a2d1b8c
Trevor <59959500+trevorcummingsCCS@users.noreply.github.com>
2020-07-16 22:01:02 +0100

    Create create_lot_suppliers.sql

---
+++ agreements/create_lot_suppliers.sql
@@ -0,0 +1,9 @@
+/*
+Title       create_organisations.sql
+Date        July 16th 2020
+Author      Trevor Cummings
+Description This file is a script to create data for ORGANISATIONS. This data eventually will reside in Conclave     
+*/
+INSERT INTO LOT_SUPPLIERS (lot_id, organisation_id, start_date, end_date)
+values
+                          (6, (select organisation_id from organisations where legal_name = 'ACS Business Supplies Ltd'),'31-OCT-2016','31-OCT-2021');
