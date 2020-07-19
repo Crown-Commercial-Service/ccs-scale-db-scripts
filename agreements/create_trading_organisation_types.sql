444b01ae36206460b5d14051970165e17c5d9da5
Trevor <59959500+trevorcummingsCCS@users.noreply.github.com>
2020-07-19 22:32:58 +0100

    Update create_trading_organisation_types.sql

--- agreements/create_trading_organisation_types.sql
+++ agreements/create_trading_organisation_types.sql
@@ -2,7 +2,7 @@
 Title       create_trading_organisation_types.sql
 Date        July 19th 2020
 Author      Trevor Cummings
-Description This file is a script to create data for ORGANISATION_TYPES. This data eventually will reside in Conclave     
+Description This file is a script to create data for trading_organisation_types. This data eventually will reside in Conclave     
 */
 INSERT INTO public.trading_organisation_types(
 	trading_organisation_type_id, trading_organisation_type_name, trading_organisation_type_description)
