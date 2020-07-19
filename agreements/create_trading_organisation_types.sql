06a8a0c4cf1d7af746039cbee75546c0f8a83f8f
Trevor <59959500+trevorcummingsCCS@users.noreply.github.com>
2020-07-19 21:32:32 +0100

    Required to return data for an organisation

---
+++ agreements/create_trading_organisation_types.sql
@@ -0,0 +1,9 @@
+/*
+Title       create_trading_organisation_types.sql
+Date        July 19th 2020
+Author      Trevor Cummings
+Description This file is a script to create data for ORGANISATION_TYPES. This data eventually will reside in Conclave     
+*/
+INSERT INTO public.trading_organisation_types(
+	trading_organisation_type_id, trading_organisation_type_name, trading_organisation_type_description)
+	VALUES (1, 'Organisation contact email', 'The contact email address for the organisation');
