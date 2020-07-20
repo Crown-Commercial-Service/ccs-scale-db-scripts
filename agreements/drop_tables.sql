4f63198a4d2e2bf90952222e392f43dd2ca89a45
Trevor <59959500+trevorcummingsCCS@users.noreply.github.com>
2020-07-20 15:55:19 +0100

    Update drop_tables.sql

--- agreements/drop_tables.sql
+++ agreements/drop_tables.sql
@@ -17,7 +17,7 @@ DROP TABLE commercial_agreement_role_types;
 
 DROP TABLE commercial_agreement_organisation_roles;
 
-DROP TABLE lot_supplier_people_roles;
+DROP TABLE lot_people_roles;
  
 DROP TABLE lot_organisation_roles;
 
