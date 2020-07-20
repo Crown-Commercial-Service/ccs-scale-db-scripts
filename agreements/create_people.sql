7889238f2829f94bfffebed8faa8a5ebb9feb6d4
Trevor <59959500+trevorcummingsCCS@users.noreply.github.com>
2020-07-20 09:31:44 +0100

    Create create_people.sql

---
+++ agreements/create_people.sql
@@ -0,0 +1,15 @@
+/*
+Title       create_people.sql
+Date        July 16th 2020
+Author      Trevor Cummings
+Description This file is a script to create data for people.  
+*/
+INSERT INTO people (organisation_id,
+                    first_name,
+                    last_name,
+                    title)
+values
+                    (1,
+                    'Linus',
+                    'Van Pelt'
+                    'Master');
