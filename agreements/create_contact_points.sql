e475622ec028375df96cf1ed1a50cc4070b8c116
Trevor <59959500+trevorcummingsCCS@users.noreply.github.com>
2020-07-20 09:34:41 +0100

    Update create_contact_points.sql
    Add party table name so that multiple party tables can be used.

--- agreements/create_contact_points.sql
+++ agreements/create_contact_points.sql
@@ -5,6 +5,6 @@ Author      Trevor Cummings
 Description This file is a script to create data for contact_points. This data eventually will reside in Conclave     
 */
 INSERT INTO public.contact_points(
-	contact_point_id, contact_method_id, contact_point_reason_id, party_id, effective_from, effecive_to, 
+	contact_point_id, contact_method_id, contact_point_reason_id, party_id, party_table_name, effective_from, effecive_to, 
 	primary_ind, source_application_system)
-	VALUES (1, 1, 1, 1, '01-Jul-2020', null, TRUE, 'BaT');
+	VALUES (1, 1, 1, 1, 'ORGANISATIONS', '01-Jul-2020', null, TRUE, 'BaT');
