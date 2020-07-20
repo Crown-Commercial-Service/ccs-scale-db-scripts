c5dc9daf5aea572adcec4f9e92f02481bdf38107
Trevor <59959500+trevorcummingsCCS@users.noreply.github.com>
2020-07-20 16:37:13 +0100

    Create create_lot_people_role_types.sql
    add data lot_people_role_types

---
+++ agreements/create_lot_people_role_types.sql
@@ -0,0 +1,8 @@
+/*
+Title       ccreate_lot_people_role_types.sql
+Date        July 20th 2020
+Author      Trevor Cummings
+Description This file is a script to create data for lot_people_role_types
+*/
+INSERT INTO LOT_PEOPLE_ROLE_TYPES (role_type_id,role_type_name)
+values (1,'Contact for the supplier in relation to the lot');
