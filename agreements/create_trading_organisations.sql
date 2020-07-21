4856ecbd9ec682336ce53d05fa8ac0213c8a6193
Trevor <59959500+trevorcummingsCCS@users.noreply.github.com>
2020-07-21 12:00:16 +0100

    Update create_trading_organisations.sql
    removed reference to deleted column

--- agreements/create_trading_organisations.sql
+++ agreements/create_trading_organisations.sql
@@ -5,5 +5,5 @@ Author      Trevor Cummings
 Description This file is a script to create data for trading_organisations. This data eventually will reside in Conclave     
 */
 INSERT INTO public.trading_organisations(
-	trading_organisation_id, trading_organisation_type_id, organisation_id, trading_organisation_name)
-	VALUES (1,1,(select organisation_id from organisations where legal_name = 'ACS Business Supplies Ltd'), 'ACS Ltd');
+	trading_organisation_id, organisation_id, trading_organisation_name)
+	VALUES (1,(select organisation_id from organisations where legal_name = 'ACS Business Supplies Ltd'), 'ACS Ltd');
