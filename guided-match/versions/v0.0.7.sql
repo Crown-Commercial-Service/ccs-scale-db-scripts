80668ddaa969569048d01025a69ffd3128d9c3f0
Trevor <59959500+trevorcummingsCCS@users.noreply.github.com>
2020-09-02 11:22:39 +0100

    Create v0.0.7.sql

---
+++ guided-match/versions/v0.0.7.sql
@@ -0,0 +1,5 @@
+/*
+Version No Version Description
+v0.0.7     This file is a script to create add an additional column to error_messages */
+
+Alter table error_messages add column message_summary varchar(2000);
