db7818bf4607f3bbafffc7a9ee379fd3ba0d8085
Trevor <59959500+trevorcummingsCCS@users.noreply.github.com>
2020-09-02 11:45:20 +0100

    Update v0.0.7.sql
    set error_message and summary to same size varchar(1000)

--- guided-match/versions/v0.0.7.sql
+++ guided-match/versions/v0.0.7.sql
@@ -2,4 +2,6 @@
 Version No Version Description
 v0.0.7     This file is a script to create add an additional column to error_messages */
 
-Alter table error_messages add column error_summary varchar(2000);
+Alter table error_messages add column error_summary varchar(1000);
+
+Alter table error_messages alter column type error_message varchar(1000);
