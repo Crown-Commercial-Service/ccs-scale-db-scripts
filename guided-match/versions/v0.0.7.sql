d6c81944acc1b04755d4c5f130d143a7e2508d40
Adrian Milne <adrian.milne@crowncommercial.gov.uk>
2020-10-06 11:38:21 +0100

    Release 1.1 - included update

--- guided-match/versions/v0.0.7.sql
+++ guided-match/versions/v0.0.7.sql
@@ -8,8 +8,7 @@ insert into failure_validation_types (failure_validation_type_code, failure_vali
 values ('checkPositiveNumber', 'User has entered zero or a negative number');
 
 insert into error_messages (error_message_code, error_summary, error_message)
-values ('ERR_GM021', 'Select where you need this service', 'Select where you need this service'),
-('ERR_GM022', 'Enter your budget using numbers above 0', 'Enter your budget using numbers above 0'),
+values ('ERR_GM022', 'Enter your budget using numbers above 0', 'Enter your budget using numbers above 0'),
 ('ERR_GM023', 'Enter the length of your contract using whole numbers', 'Enter the length of your contract using whole numbers'),
 ('ERR_GM024', 'Enter the length of your contract using numbers above 0', 'Enter the length of your contract using numbers above 0'),
 ('ERR_GM025', 'Enter the length of your contract in months', 'Enter the length of your contract in months');
@@ -20,3 +19,5 @@ values ('checkPositiveNumber', 'ERR_GM022', 'b879c16c-654e-11ea-bc55-0242ac13000
 ('noValue', 'ERR_GM025', 'b879c25c-654e-11ea-bc55-0242ac130003'), --Contract
 ('checkWholeNumber', 'ERR_GM023', 'b879c25c-654e-11ea-bc55-0242ac130003'), --Contract
 ('checkPositiveNumber', 'ERR_GM024', 'b879c25c-654e-11ea-bc55-0242ac130003'); --Contract
+
+update error_messages set error_message = 'Select which service you need' where error_message_code = 'ERR_GM011';
