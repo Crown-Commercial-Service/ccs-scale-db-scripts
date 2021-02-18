88c248dba7d9e7c9e97ec50815afc9237211b515
Trevor <59959500+trevorcummingsCCS@users.noreply.github.com>
2021-02-18 14:32:16 +0000

    Update drop_tables.sql

--- agreements/drop_tables.sql
+++ agreements/drop_tables.sql
@@ -9,51 +9,51 @@ Description This file is a script to drop the tables for the Agreements database
 
 */
 
-DROP TABLE commercial_agreement_organisation_roles;
+DROP TABLE IF EXISTS commercial_agreement_organisation_roles;
 
-DROP TABLE lot_people_roles;
+DROP TABLE IF EXISTS lot_people_roles;
  
-DROP TABLE lot_organisation_roles;
+DROP TABLE IF EXISTS lot_organisation_roles;
 
-DROP TABLE people;
+DROP TABLE IF EXISTS people;
 
-DROP TABLE trading_organisations ; 
+DROP TABLE IF EXISTS trading_organisations ; 
 
-DROP TABLE organisations;
+DROP TABLE IF EXISTS organisations;
 
-DROP TABLE lot_route_to_market;
+DROP TABLE IF EXISTS lot_route_to_market;
 
-DROP TABLE route_to_market;
+DROP TABLE IF EXISTS route_to_market;
 
-DROP TABLE lot_sectors;
+DROP TABLE IF EXISTS lot_sectors;
 
-DROP TABLE sectors;
+DROP TABLE IF EXISTS sectors;
 
-DROP TABLE contact_points;
+DROP TABLE IF EXISTS contact_points;
 
-DROP TABLE contact_point_reasons; 
+DROP TABLE IF EXISTS contact_point_reasons; 
 
-DROP TABLE contact_methods; 
+DROP TABLE IF EXISTS contact_methods; 
 
-DROP TABLE contact_method_types;
+DROP TABLE IF EXISTS contact_method_types;
 
-DROP TABLE commercial_agreement_contacts;
+DROP TABLE IF EXISTS commercial_agreement_contacts;
 
-DROP TABLE lot_rule_transaction_objects;
+DROP TABLE IF EXISTS lot_rule_transaction_objects;
 
-DROP TABLE lot_rule_attributes;
+DROP TABLE IF EXISTS lot_rule_attributes;
 
-DROP TABLE lot_related_lots;
+DROP TABLE IF EXISTS lot_related_lots;
 
-DROP TABLE lot_rules;
+DROP TABLE IF EXISTS lot_rules;
 
-DROP TABLE lots;
+DROP TABLE IF EXISTS lots;
 
-DROP TABLE commercial_agreements;
+DROP TABLE IF EXISTS commercial_agreements;
 
-DROP TABLE lot_people_role_types;
+DROP TABLE IF EXISTS lot_people_role_types;
 
-DROP TABLE commercial_agreement_role_types;
+DROP TABLE IF EXISTS commercial_agreement_role_types;
 
-DROP TABLE lot_organisation_role_types;
+DROP TABLE IF EXISTS lot_organisation_role_types;
 
