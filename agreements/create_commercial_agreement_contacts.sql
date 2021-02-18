4accde3f93e2c1e6f53aa23684587080be621de0
Trevor <59959500+trevorcummingsCCS@users.noreply.github.com>
2021-02-18 16:05:37 +0000

    Delete create_commercial_agreement_contacts.sql

--- agreements/create_commercial_agreement_contacts.sql
+++
@@ -1,8 +0,0 @@
-/*
-Title       create_commercial_agreement_contacts.sql
-Date        June 23rd 2020
-Author      Trevor Cummings
-Description This file is a script to create data for lot rule transaction objects       
-*/
-INSERT INTO commercial_agreement_contacts(commercial_agreement_id, contact_type, email_address)
-values                                   (1                      ,'CCSCategoryManager','test_scale@crowncommercial.gov.uk');
