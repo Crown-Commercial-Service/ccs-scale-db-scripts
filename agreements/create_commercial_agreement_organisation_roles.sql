125ff00b3cdecb628567246fccf685884a816b6c
Trevor <59959500+trevorcummingsCCS@users.noreply.github.com>
2021-02-26 15:01:09 +0000

    Update create_commercial_agreement_organisation_roles.sql
    Consolidated role type tables

--- agreements/create_commercial_agreement_organisation_roles.sql
+++ agreements/create_commercial_agreement_organisation_roles.sql
@@ -8,5 +8,5 @@ Description This file is a script to create data for COMMERCIAL_AGREEMENT_ORGANI
 INSERT INTO COMMERCIAL_AGREEMENT_ORGANISATION_ROLES (commercial_agreement_id, organisation_id, role_type_id,start_date, end_date)
 values
                           (1, (select organisation_id from organisations where legal_name = 'ACS Business Supplies Ltd'),
-						   1,'31-OCT-2016','31-OCT-2021');
+						   3,'31-OCT-2016','31-OCT-2021');
 						  
