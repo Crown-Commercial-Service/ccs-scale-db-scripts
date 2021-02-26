1c7df3789ec6a41e757f73cd95bc4defc96005fc
Trevor <59959500+trevorcummingsCCS@users.noreply.github.com>
2021-02-26 14:50:51 +0000

    Update create_commercial_agreement_role_types.sql

--- agreements/create_commercial_agreement_role_types.sql
+++ agreements/create_commercial_agreement_role_types.sql
@@ -1,9 +1,14 @@
 /*
 Title       create_commercial_agreement_role_types.sql
-Date        July 20th 2020
+Date        Feb 26th 2021
 Author      Trevor Cummings
-Description This file is a script to create data for Lot_organisation_role_types   
+Description This file is a script to create data for role_types   
 */
-INSERT INTO COMMERCIAL_AGREEMENT_ROLE_TYPES(role_type_id,role_type_name)
-values
-(                                       1,'Owner');
+INSERT INTO ROLE_TYPES (role_type_id,role_domain,role_type_name)
+values (1,'LOT_PEOPLE','Contact for the supplier in relation to the lot');
+
+INSERT INTO ROLE_TYPES (role_type_id,role_domain,role_type_name)
+values (2,'LOT_ORGANISATION','Supplier');
+
+INSERT INTO ROLE_TYPES (role_type_id,role_domain,role_type_name)
+values (3,'COMMERCIAL_AGREEMENT_ORGANISATION','Owner');
