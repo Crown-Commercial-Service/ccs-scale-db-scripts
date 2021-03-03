34ec004301aad7315d2b8282f51ef6cffaa4d9d0
Trevor <59959500+trevorcummingsCCS@users.noreply.github.com>
2021-03-03 14:37:55 +0000

    Update create_contact_details.sql

--- agreements/create_contact_details.sql
+++ agreements/create_contact_details.sql
@@ -7,5 +7,5 @@ Description This file is a script to create data for contact_details. This data 
 INSERT INTO public.contact_details(
 	contact_detail_id, effective_from, effective_to, street_address, locality, region, postal_code, country_code, uprn, 
 	telephone_number, fax_number, email_address, url)
-	VALUES (1, '01-Jan-2016', null, '1 Waterdale', 'Wombourne', ['Wolverhampton', 'WV5 0DH', null, null,
+	VALUES (1, '01-Jan-2016', null, '1 Waterdale', 'Wombourne', 'Wolverhampton', 'WV5 0DH', null, null,
 		'020020202', '03030303', 'test_bat@crowncommercial.gov.uk','www.crowncommercial.gov.uk');
