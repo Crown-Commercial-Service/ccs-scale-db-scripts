3fc36c0ee540e617a657b731245664a997cf4a10
Trevor <59959500+trevorcummingsCCS@users.noreply.github.com>
2021-03-03 14:32:11 +0000

    Update create_contact_details.sql

--- agreements/create_contact_details.sql
+++ agreements/create_contact_details.sql
@@ -5,6 +5,7 @@ Author      Trevor Cummings
 Description This file is a script to create data for contact_details. This data eventually will reside in Conclave     
 */
 INSERT INTO public.contact_details(
-	contact_detail_id, contact_method_type_id, effective_from, effective_to, 
-	virtual_address_value, street_address, locality, region, postal_code, country_code, uprn)
-	VALUES (1, 1, '01-Jan-2016', null, 'test_bat@crowncommercial.gov.uk', null, null, null, null, null, null);
+	contact_detail_id, effective_from, effective_to, street_address, locality, region, postal_code, country_code, uprn, 
+	telephone_number, fax_number, email_address, url)
+	VALUES (1, '01-Jan-2016', null, '1 Waterdale', 'Wombourne', ['Wolverhampton', 'WV5 0DH', null, null,
+		'020020202', '03030303', 'test_bat@crowncommercial.gov.uk','www.crowncommercial.gov.uk');
