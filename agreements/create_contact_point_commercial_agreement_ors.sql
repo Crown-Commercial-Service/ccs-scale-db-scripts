77fc4e04c7dcd45cbeb3d35c5a09e28e5544db2a
tommyb82 <thomasbunting@gmail.com>
2021-03-09 11:48:39 +0000

    Fix typo acros remaining files

--- agreements/create_contact_point_commercial_agreement_ors.sql
+++ agreements/create_contact_point_commercial_agreement_ors.sql
@@ -5,5 +5,5 @@ Author      Trevor Cummings
 Description This file is a script to create data for contact_point_lot_prs. This data eventually will reside in Conclave     
 */
 
-INSERT INTO public.contact_point_commercial_agreement_ors(contact_detail_id, contact_point_reason_id, commercial_agreement_organisation_role_id, effective_from, effecive_to, primary_ind)
+INSERT INTO public.contact_point_commercial_agreement_ors(contact_detail_id, contact_point_reason_id, commercial_agreement_organisation_role_id, effective_from, effective_to, primary_ind)
 	VALUES ( 1, 1, 1, '01-Jul-2020', null, TRUE);		
