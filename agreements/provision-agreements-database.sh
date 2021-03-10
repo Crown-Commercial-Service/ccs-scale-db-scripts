7da85406b0544fab8daf8ef0592c6ba45ff401a2
Trevor <59959500+trevorcummingsCCS@users.noreply.github.com>
2021-03-10 11:27:47 +0000

    Update provision-agreements-database.sh (#57)
    removed the create data calls

--- agreements/provision-agreements-database.sh
+++ agreements/provision-agreements-database.sh
@@ -20,28 +20,5 @@ export USERNAME=<<FILL THIS IN>>
 # psql -h $SERVER -d $DATABASE -p $PORT -U $USERNAME -a -q -f drop_tables.sql
 psql -h $SERVER -d $DATABASE -p $PORT -U $USERNAME -a -q -f ddl.sql
 
-psql -h $SERVER -d $DATABASE -p $PORT -U $USERNAME -a -q -f create_commercial_agreements.sql
-psql -h $SERVER -d $DATABASE -p $PORT -U $USERNAME -a -q -f create_role_types.sql
-psql -h $SERVER -d $DATABASE -p $PORT -U $USERNAME -a -q -f create_contact_details.sql
-psql -h $SERVER -d $DATABASE -p $PORT -U $USERNAME -a -q -f create_contact_point_reasons.sql
-psql -h $SERVER -d $DATABASE -p $PORT -U $USERNAME -a -q -f create_lots.sql
-psql -h $SERVER -d $DATABASE -p $PORT -U $USERNAME -a -q -f create_organisations.sql
-psql -h $SERVER -d $DATABASE -p $PORT -U $USERNAME -a -q -f create_people.sql
-psql -h $SERVER -d $DATABASE -p $PORT -U $USERNAME -a -q -f create_route_to_market.sql
-psql -h $SERVER -d $DATABASE -p $PORT -U $USERNAME -a -q -f create_sectors.sql
-psql -h $SERVER -d $DATABASE -p $PORT -U $USERNAME -a -q -f create_trading_organisations.sql
-psql -h $SERVER -d $DATABASE -p $PORT -U $USERNAME -a -q -f create_commercial_agreement_organisation_roles.sql
-psql -h $SERVER -d $DATABASE -p $PORT -U $USERNAME -a -q -f create_lot_organisation_roles.sql
-psql -h $SERVER -d $DATABASE -p $PORT -U $USERNAME -a -q -f create_lot_people_roles.sql
-psql -h $SERVER -d $DATABASE -p $PORT -U $USERNAME -a -q -f create_lot_rules.sql
-psql -h $SERVER -d $DATABASE -p $PORT -U $USERNAME -a -q -f create_lot_related_lots.sql
-psql -h $SERVER -d $DATABASE -p $PORT -U $USERNAME -a -q -f create_lot_rule_attributes.sql
-psql -h $SERVER -d $DATABASE -p $PORT -U $USERNAME -a -q -f create_lot_rule_transaction_objects.sql
-psql -h $SERVER -d $DATABASE -p $PORT -U $USERNAME -a -q -f create_lot_sectors.sql 
-psql -h $SERVER -d $DATABASE -p $PORT -U $USERNAME -a -q -f create_lot_route_to_market.sql
-psql -h $SERVER -d $DATABASE -p $PORT -U $USERNAME -a -q -f create_commercial_agreement_documents.sql
-psql -h $SERVER -d $DATABASE -p $PORT -U $USERNAME -a -q -f create_contact_point_lot_prs.sql
-psql -h $SERVER -d $DATABASE -p $PORT -U $USERNAME -a -q -f create_contact_point_lot_ors.sql
-psql -h $SERVER -d $DATABASE -p $PORT -U $USERNAME -a -q -f create_contact_point_commercial_agreement_ors.sql
 
 
