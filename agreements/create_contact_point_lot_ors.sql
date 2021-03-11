c6564a852c7c148814c5d4f24ab14f9e101ca74a
Trevor <59959500+trevorcummingsCCS@users.noreply.github.com>
2021-03-11 15:21:46 +0000

    Delete create_contact_point_lot_ors.sql

--- agreements/create_contact_point_lot_ors.sql
+++
@@ -1,9 +0,0 @@
-/*
-Title       create_contact_point_lot_ors.sql
-Date        February 25th 2021
-Author      Trevor Cummings
-Description This file is a script to create data for contact_point_lot_prs. This data eventually will reside in Conclave     
-*/
-INSERT INTO public.contact_point_lot_ors(contact_detail_id, contact_point_reason_id, lot_organisation_role_id, effective_from, effective_to, primary_ind)
-	VALUES ( 1, 1, 1, '01-Jul-2020', null, TRUE);
-	
