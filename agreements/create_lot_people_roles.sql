d46de77e60d2159bc8c818b81fa629e67a9ac029
Trevor <59959500+trevorcummingsCCS@users.noreply.github.com>
2021-03-11 15:22:34 +0000

    Delete create_lot_people_roles.sql

--- agreements/create_lot_people_roles.sql
+++
@@ -1,9 +0,0 @@
-/*
-Title       create_lot_people_roles.sql
-Date        July 20th 2020
-Author      Trevor Cummings
-Description This file is a script to create data for lot_people_roles.   
-*/
-
-INSERT INTO LOT_PEOPLE_ROLES (lot_id, person_id,role_type_id,start_date,end_date)
-values                       (6,1,1,'31-OCT-2016',null)
