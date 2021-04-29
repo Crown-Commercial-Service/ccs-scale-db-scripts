1a64a1753f4d3c67ed20e10e86141561f353352e
Trevor <59959500+trevorcummingsCCS@users.noreply.github.com>
2021-04-29 10:56:23 +0100

    Update ddl.sql

--- tenders-api/ddl.sql
+++ tenders-api/ddl.sql
@@ -9,12 +9,23 @@ Version No Version Description
 V0.0.1       Initial Release
 */
 
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+
 CREATE TABLE procurement_projects (project_id                  UUID PRIMARY KEY,
                                    commercial_agreement_number VARCHAR(20)   NOT NULL,
                                    lot_number                  VARCHAR(20)   NOT NULL,
-                                   internal_jaggaer_id         VARCHAR(2000) NOT NULL,
+                                   jaggaer_project_id          VARCHAR(2000) NOT NULL,
                                    project_name                VARCHAR(2000),
                                    created_by                  VARCHAR(2000) NOT NULL,
                                    created_at                  TIMESTAMP,
                                    updated_by                  VARCHAR(2000) NOT NULL,
                                    updated_at                  TIMESTAMP);
+                                   
+CREATE TABLE procurement_events  (event_id         VARCHAR(100) PRIMARY KEY, -- This comprises of the OCID as the prfix and an a unique id for the procurement
+                                  jaggaer_event_id VARCHAR(2000) NOT NULL,
+                                  event_name       VARCHAR(2000), 
+                                  created_by       VARCHAR(2000) NOT NULL,
+                                  created_at       TIMESTAMP,
+                                  updated_by       VARCHAR(2000) NOT NULL,
+                                  updated_at       TIMESTAMP);
