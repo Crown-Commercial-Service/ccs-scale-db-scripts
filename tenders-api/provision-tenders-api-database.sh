10569dcaf3b57cd277a7764d14839fc1b4f0c23d
Trevor <59959500+trevorcummingsCCS@users.noreply.github.com>
2021-04-29 11:05:44 +0100

    Create provision-tenders-api-database.sh

---
+++ tenders-api/provision-tenders-api-database.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Create and Populate an tenders-api Database.
+# 
+# Prerequisites:
+#   - Create ~/.pgpass file to hold authentication details
+#     - *:*:*:sysadmin:PASSWORD
+#     - https://www.postgresql.org/docs/current/libpq-pgpass.html
+#   - An SSH tunnel has been established to the remote database via an EC2 bastion host, such that it appears as a local DB
+#     - Using a command similar to:
+#     - ssh -i  {ENV}-bastion-key.pem -L 5432:{RDS_ENDPOINT}:5432 ubuntu@{BASTION_EC2_IP}
+
+
+export SERVER=localhost
+export PORT=5432
+export DATABASE=agreements
+export USERNAME=<<FILL THIS IN>>
+
+psql -h $SERVER -d $DATABASE -p $PORT -U $USERNAME -a -q -f ddl.sql
