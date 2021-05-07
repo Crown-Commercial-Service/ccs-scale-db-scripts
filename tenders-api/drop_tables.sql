be95763bebe552fdbbb0e1c0f8cd013f7edce084
Trevor <59959500+trevorcummingsCCS@users.noreply.github.com>
2021-05-07 07:47:39 +0100

    Create ddl.sql (#61)
    * Create ddl.sql
    
    * Update ddl.sql
    
    * Create drop_tables.sql
    
    * Update ddl.sql
    
    * Create provision-tenders-api-database.sh
    
    * Update ddl.sql
    
    Updated project id from UUID to serial in table procurement_projects. Changed event id to serial from varchar in table procure_events.
    
    * Update ddl.sql
    
    * Update ddl.sql
    
    changed ocds_prefix to ocid_prefix

---
+++ tenders-api/drop_tables.sql
@@ -0,0 +1,10 @@
+/*
+Title       Drop table script for tenders-api
+Date        April 29th 2021
+Author      Trevor Cummings
+Description This file is a script to drop the tables for Tenders-ai.
+*/
+
+DROP TABLE IF EXISTS procurement_events;
+
+DROP TABLE IF EXISTS procurement_projects;
