7ee62f303c4cf9f8fd5d90964d12353ad8c93a34
Trevor Cummings <trevorcummings@Trevors-Air.lan>
2021-05-27 12:23:12 +0100

    set columns holding default values to optional so they could remain null

--- agreements/ddl.sql
+++ agreements/ddl.sql
@@ -124,10 +124,10 @@ CREATE INDEX LOT_RELATED_LOTS_IDX1 on LOT_RELATED_LOTS (lot_rule_id);
 
 CREATE TABLE organisations (
   organisation_id          SERIAL         NOT NULL PRIMARY KEY,
-  entity_id                VARCHAR(255)   NOT NULL,
+  entity_id                VARCHAR(255),
   registry_code            VARCHAR(20), 	
   legal_name               VARCHAR(255)   NOT NULL UNIQUE,
-  business_type            VARCHAR(20)        NOT NULL,
+  business_type            VARCHAR(20)    ,
   organisation_uri         VARCHAR(2000),
   status                   VARCHAR(100),
   incorporation_date       DATE           NOT NULL,
