7e19388d4e3b698aa4e9915270fcb4fe911cb5e2
Trevor Cummings <trevorcummings@Trevors-Air.lan>
2021-08-04 17:03:05 +0100

    added columns for SCC-530

--- tenders-api/ddl.sql
+++ tenders-api/ddl.sql
@@ -23,17 +23,19 @@ CREATE TABLE procurement_projects (project_id                  SERIAL PRIMARY KE
                                    updated_by                  VARCHAR(2000) NOT NULL,
                                    updated_at                  TIMESTAMP);
                                    
-CREATE TABLE procurement_events  (event_id              SERIAL PRIMARY KEY, -- This comprises of the OCID as the prfix and an a unique id for the procurement
-                                  ocds_authority_name   VARCHAR(20) NOT NULL, -- default will be OCDS and probably never change
-                                  ocid_prefix           VARCHAR(50) NOT NULL, -- current value is b5fd17
-                                  external_event_id     VARCHAR(50) NOT NULL,
-                                  external_reference_id VARCHAR(50) NOT NULL,
-                                  project_id            INTEGER     NOT NULL,
-                                  event_name            VARCHAR(2000), 
-                                  created_by            VARCHAR(2000) NOT NULL,
-                                  created_at            TIMESTAMP,
-                                  updated_by            VARCHAR(2000) NOT NULL,
-                                  updated_at            TIMESTAMP);
+CREATE TABLE procurement_events  (event_id                    SERIAL PRIMARY KEY, -- 
+                                  ocds_authority_name         VARCHAR(20) NOT NULL, -- default will be OCDS and probably never change
+                                  ocid_prefix                 VARCHAR(50) NOT NULL, -- current value is b5fd17
+                                  external_event_id           VARCHAR(50) NOT NULL,
+                                  external_reference_id       VARCHAR(50) NOT NULL,
+                                  project_id                  INTEGER     NOT NULL,
+                                  event_name                  VARCHAR(2000), 
+                                  event_type                  VARCHAR(20) NOT NULL,
+                                  down_selected_suppliers_ind BOOLEAN NOT NULL,
+                                  created_by                  VARCHAR(2000) NOT NULL,
+                                  created_at                  TIMESTAMP,
+                                  updated_by                  VARCHAR(2000) NOT NULL,
+                                  updated_at                  TIMESTAMP);
                                   
 ALTER TABLE procurement_events
 ADD CONSTRAINT procurement_event_procurement_project_fk FOREIGN KEY (project_id)
