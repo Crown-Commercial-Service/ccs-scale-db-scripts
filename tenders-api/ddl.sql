da32b89510ce8984a9efbb31bf1a779fdddc332f
Trevor <59959500+trevorcummingsCCS@users.noreply.github.com>
2021-09-28 20:58:55 +0100

    Added table procurement_event_history
    added the table procurement_event_history

--- tenders-api/ddl.sql
+++ tenders-api/ddl.sql
@@ -23,20 +23,35 @@ CREATE TABLE procurement_projects (project_id                  SERIAL PRIMARY KE
                                    updated_by                  VARCHAR(2000) NOT NULL,
                                    updated_at                  TIMESTAMP);
                                    
-CREATE TABLE procurement_events  (event_id                    SERIAL PRIMARY KEY, -- 
-                                  ocds_authority_name         VARCHAR(20) NOT NULL, -- default will be OCDS and probably never change
-                                  ocid_prefix                 VARCHAR(50) NOT NULL, -- current value is b5fd17
-                                  external_event_id           VARCHAR(50) NOT NULL,
-                                  external_reference_id       VARCHAR(50) NOT NULL,
-                                  project_id                  INTEGER     NOT NULL,
-                                  event_name                  VARCHAR(2000), 
-                                  event_type                  VARCHAR(20) NOT NULL,
-                                  down_selected_suppliers_ind BOOLEAN NOT NULL,
-                                  created_by                  VARCHAR(2000) NOT NULL,
-                                  created_at                  TIMESTAMP,
-                                  updated_by                  VARCHAR(2000) NOT NULL,
-                                  updated_at                  TIMESTAMP);
+CREATE TABLE procurement_events  (event_id                     SERIAL PRIMARY KEY, -- 
+                                  ocds_authority_name          VARCHAR(20) NOT NULL, -- default will be OCDS and probably never change
+                                  ocid_prefix                  VARCHAR(50) NOT NULL, -- current value is b5fd17
+                                  external_event_id            VARCHAR(50) NOT NULL,
+                                  external_reference_id        VARCHAR(50) NOT NULL,
+                                  project_id                   INTEGER     NOT NULL,
+                                  event_name                   VARCHAR(2000), 
+                                  event_type                   VARCHAR(20) NOT NULL,
+                                  down_selected_suppliers_ind  BOOLEAN NOT NULL,
+		          procurement_template_payload JSONB,
+                                  created_by                   VARCHAR(2000) NOT NULL,
+                                  created_at                   TIMESTAMP,
+                                  updated_by                   VARCHAR(2000) NOT NULL,
+                                  updated_at                   TIMESTAMP);
                                   
+CREATE TABLE procurement_event_history (event_history_id             SERIAL PRIMARY KEY, -- 
+                                        event_id                     INTEGER NOT NULL,
+			    procurement_template_payload JSONB,
+                                        created_by                   VARCHAR(2000) NOT NULL,
+                                        created_at                   TIMESTAMP,
+                                        updated_by                   VARCHAR(2000) NOT NULL,
+                                        updated_at                   TIMESTAMP);
+                                  
+
 ALTER TABLE procurement_events
 ADD CONSTRAINT procurement_event_procurement_project_fk FOREIGN KEY (project_id)
-    REFERENCES procurement_projects(project_id);                                  
+    REFERENCES procurement_projects(project_id);  
+	
+ALTER TABLE procurement_event_history
+ADD CONSTRAINT procurement_event_history_procurement_event_fk FOREIGN KEY (event_id)
+    REFERENCES procurement_events(event_id);  	
+	
