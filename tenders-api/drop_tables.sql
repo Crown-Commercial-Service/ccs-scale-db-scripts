ebd8b9de69d6c4eff3dfeb05b30234b40d6ad956
Trevor <59959500+trevorcummingsCCS@users.noreply.github.com>
2021-09-29 12:02:50 +0100

    SCAT-1693 (#68)
    * SCAT-1693
    
    Have added the drop command for the new table procurement_event_history
    
    * Added table procurement_event_history
    
    added the table procurement_event_history

--- tenders-api/drop_tables.sql
+++ tenders-api/drop_tables.sql
@@ -5,6 +5,8 @@ Author      Trevor Cummings
 Description This file is a script to drop the tables for Tenders-ai.
 */
 
+DROP TABLE IF EXISTS procurement_event_history;
+
 DROP TABLE IF EXISTS procurement_events;
 
 DROP TABLE IF EXISTS procurement_projects;
