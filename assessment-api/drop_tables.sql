230989665c35dd8c320f23f1337b1664007e6cf9
Trevor Cummings <trevorcummings@Trevors-Air.lan>
2022-01-14 16:09:32 +0000

    Initial assessmen-api load

---
+++ assessment-api/drop_tables.sql
@@ -0,0 +1,24 @@
+-- Prototype database design for Cabability assessment
+-- load tables
+
+drop table IF EXISTS cap_load_resources;
+drop table IF EXISTS cap_load_service_capability;
+drop table IF EXISTS cap_load_jobs;
+
+-- Create table cap_dimensions
+
+
+drop table IF EXISTS cap_dimensions;
+drop table IF EXISTS cap_dimension_valid_values;
+drop table IF EXISTS cap_submission_types;
+drop table IF EXISTS cap_taxonomies;
+drop table IF EXISTS cap_taxons;
+drop table IF EXISTS cap_taxon_dimensions;
+drop table IF EXISTS cap_products;
+drop table IF EXISTS cap_product_taxons;
+drop table IF EXISTS cap_lot_product_taxons;
+drop table IF EXISTS cap_supplier_submissions;
+
+
+
+
