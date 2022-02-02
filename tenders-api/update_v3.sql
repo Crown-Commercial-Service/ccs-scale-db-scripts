2c93074deacf26a97ecd79294f038df70f8c10bf
Tom Bunting <6031257+tommyb82@users.noreply.github.com>
2022-02-02 14:32:18 +0000

    Add assessment_id to proc event table (#92)

--- tenders-api/update_v3.sql
+++ tenders-api/update_v3.sql
@@ -2,3 +2,4 @@
 Tenders DB Update script: v3: Add assessment supplier target (numeric) to proc events
 */
 ALTER TABLE procurement_events ADD COLUMN IF NOT EXISTS assessment_supplier_target INTEGER NULL;
+ALTER TABLE procurement_events ADD COLUMN IF NOT EXISTS assessment_id INTEGER NULL;
