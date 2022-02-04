b599cb9cb26608fcf73fd00d7c0ab276cacde25a
tommyb82 <thomasbunting@gmail.com>
2022-02-04 14:09:55 +0000

    Tenders DB Update 4

--- tenders-api/provision-tenders-api-database.sh
+++ tenders-api/provision-tenders-api-database.sh
@@ -4,17 +4,24 @@
 # 
 # Prerequisites:
 #   - Create ~/.pgpass file to hold authentication details
-#     - *:*:*:sysadmin:PASSWORD
+#     - *:*:database:username:password
 #     - https://www.postgresql.org/docs/current/libpq-pgpass.html
-#   - An SSH tunnel has been established to the remote database via an EC2 bastion host, such that it appears as a local DB
+#   - An SSH tunnel has been established to the remote database via the CF CLI, such that it appears as a local DB
 #     - Using a command similar to:
-#     - ssh -i  {ENV}-bastion-key.pem -L 5432:{RDS_ENDPOINT}:5432 ubuntu@{BASTION_EC2_IP}
+#     cf ssh sbx2-ccs-scale-cat-nginx -L 5432:rdsbroker-abc123.eu-west-2.rds.amazonaws.com:5432
+#
+# Then run the script specifying the database and username to use:
+# $ export USERNAME=...
+# $ export DATABASE=...
+# $ ./provision-tenders-api-database.sh
+# 
 
-
-export SERVER=localhost
+export SERVER=127.0.0.1
 export PORT=5432
-export DATABASE=agreements
-export USERNAME=<<FILL THIS IN>>
 
 psql -h $SERVER -d $DATABASE -p $PORT -U $USERNAME -a -q -f drop_tables.sql
 psql -h $SERVER -d $DATABASE -p $PORT -U $USERNAME -a -q -f ddl.sql
+psql -h $SERVER -d $DATABASE -p $PORT -U $USERNAME -a -q -f udpate_v1.sql
+psql -h $SERVER -d $DATABASE -p $PORT -U $USERNAME -a -q -f udpate_v2.sql
+psql -h $SERVER -d $DATABASE -p $PORT -U $USERNAME -a -q -f udpate_v3.sql
+psql -h $SERVER -d $DATABASE -p $PORT -U $USERNAME -a -q -f udpate_v4.sql
