c81b319c71498242500f2b2082a251d0a1a5dd27
Sudhakar Konda <sudhakar.konda@crowncommercial.gov.uk>
2022-02-21 19:10:47 +0000

    SCAT-4251 new project user mapping table

--- tenders-api/update_v5.sql
+++ tenders-api/update_v5.sql
@@ -1,20 +1,18 @@
-
 /*
-Tenders DB Update script: v5 (project_user_mapping table)
+Tenders DB Update script: v5: Add supplier_selections table
 */
-CREATE TABLE IF NOT EXISTS project_user_mapping (
-    project_id                   INTEGER     NOT NULL,
-    event_id                     INTEGER NOT NULL,
-    event_type                   VARCHAR(20) NOT NULL,
-    jaggaer_id                   VARCHAR(20) NOT NULL,
-    created_at                   TIMESTAMP   NOT NULL,
-    updated_at                   TIMESTAMP,
-    PRIMARY KEY (project_id,event_id)  );
 
-ALTER TABLE project_user_mapping
-ADD CONSTRAINT project_user_mapping_project_fk FOREIGN KEY (project_id)
-    REFERENCES procurement_projects(project_id);
+CREATE TABLE IF NOT EXISTS supplier_selections (
+    supplier_selection_id       SERIAL PRIMARY KEY,
+    event_id                    INTEGER NOT NULL,
+    organisation_mapping_id     INTEGER NOT NULL,
+    created_by                  VARCHAR(2000) NOT NULL,
+    created_at                  TIMESTAMP NOT NULL);
 
-ALTER TABLE project_user_mapping
-ADD CONSTRAINT project_user_mapping_event_fk FOREIGN KEY (event_id)
-    REFERENCES procurement_events(event_id);
+ALTER TABLE supplier_selections
+ADD CONSTRAINT supplier_selections_procurement_events_fk FOREIGN KEY (event_id)
+    REFERENCES procurement_events (event_id);
+
+ALTER TABLE supplier_selections
+ADD CONSTRAINT supplier_selections_organisation_mapping_fk FOREIGN KEY (organisation_mapping_id)
+    REFERENCES organisation_mapping (organisation_mapping_id);
