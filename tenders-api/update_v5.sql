923a2da549ff08d54530f807a9fdf26d511aa865
Sudhakar Konda <sudhakar.konda@crowncommercial.gov.uk>
2022-02-21 19:00:39 +0000

    SCAT-4251 new project user mapping table

--- tenders-api/update_v5.sql
+++ tenders-api/update_v5.sql
@@ -8,4 +8,13 @@ CREATE TABLE IF NOT EXISTS project_user_mapping (
     event_type                   VARCHAR(20) NOT NULL,
     jaggaer_id                   VARCHAR(20) NOT NULL,
     created_at                   TIMESTAMP   NOT NULL,
-    updated_at                   TIMESTAMP);
+    updated_at                   TIMESTAMP,
+    PRIMARY KEY (project_id,event_id)  );
+
+ALTER TABLE project_user_mapping
+ADD CONSTRAINT project_user_mapping_project_fk FOREIGN KEY (project_id)
+    REFERENCES procurement_projects(project_id);
+
+ALTER TABLE project_user_mapping
+ADD CONSTRAINT project_user_mapping_event_fk FOREIGN KEY (event_id)
+    REFERENCES procurement_events(event_id);
