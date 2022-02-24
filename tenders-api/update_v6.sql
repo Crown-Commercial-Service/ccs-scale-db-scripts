c72f6437ceddc3949dd2df3218c13d8959790859
Sudhakar Konda <sudhakar.konda@crowncommercial.gov.uk>
2022-02-24 13:52:57 +0000

    SCAT-4251 - remove event and event type

--- tenders-api/update_v6.sql
+++ tenders-api/update_v6.sql
@@ -4,7 +4,7 @@ Tenders DB Update script: v6 (project_user_mapping table)
 */
 CREATE TABLE IF NOT EXISTS project_user_mapping (
     project_user_mapping_id      SERIAL PRIMARY KEY,
-    event_id                     INTEGER NOT NULL,
+    project_id                   INTEGER NOT NULL,
     user_id                      VARCHAR(20) NOT NULL,
     created_by                   VARCHAR(50) NOT NULL,
     created_at                   TIMESTAMP   NOT NULL,
@@ -12,5 +12,5 @@ CREATE TABLE IF NOT EXISTS project_user_mapping (
     updated_at                   TIMESTAMP );
 
 ALTER TABLE project_user_mapping
-ADD CONSTRAINT project_user_mapping_event_fk FOREIGN KEY (event_id)
-    REFERENCES procurement_events(event_id);
+ADD CONSTRAINT project_user_mapping_project_fk FOREIGN KEY (project_id)
+    REFERENCES procurement_projects(project_id);
