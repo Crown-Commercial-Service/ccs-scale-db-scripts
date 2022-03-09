225c21398128017ea32ea63a31c52c4b7f9d8894
venkibric <venki.bathula@brickendon.com>
2022-03-09 16:17:33 +0000

    changed data lenght

--- tenders-api/update_v7.sql
+++ tenders-api/update_v7.sql
@@ -5,9 +5,9 @@ Tenders DB Update script: v7 (buyer_user_details table)
 
 CREATE TABLE public.buyer_user_details (
 	user_id varchar(100) NOT NULL,
-	user_password varchar(2000) NOT NULL,
+	user_password varchar(256) NOT NULL,
 	created_by varchar(2000) NOT NULL,
-	created_at timestamp NULL,
-	updated_by varchar(2000) NOT NULL,
+	created_at timestamp NOT NULL,
+	updated_by varchar(2000) NULL,
 	updated_at timestamp NULL
 );
