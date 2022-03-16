5ce4847150c4efaf60042a04b4dd9c49b71178f3
tommyb82 <thomasbunting@gmail.com>
2022-03-16 13:43:26 +0000

    Update v8 (new table document_uploads)

---
+++ tenders-api/update_v8.sql
@@ -0,0 +1,18 @@
+/*
+Tenders DB Update script: v8 (Document uploads table)
+*/
+
+CREATE TABLE IF NOT EXISTS document_uploads (
+    document_upload_id SERIAL PRIMARY KEY,
+    procurement_event_id INTEGER NOT NULL,
+    tenders_s3_object_key VARCHAR(1024) NOT NULL,
+    external_document_id VARCHAR(256) NULL, -- Document upload service ID
+    external_status VARCHAR(64) NOT NULL, -- Document upload service state
+    created_by VARCHAR(2000) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_by VARCHAR(2000),
+    updated_at TIMESTAMP,
+
+    -- Constraints
+    CONSTRAINT procurement_events_fk FOREIGN KEY(procurement_event_id) REFERENCES procurement_events(event_id),
+    CONSTRAINT check_external_status CHECK (external_status IN ('PROCESSING', 'SAFE', 'UNSAFE')));
