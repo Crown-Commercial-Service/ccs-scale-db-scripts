f6d6eb328b0784af1c0f67c7b48eb08b40a7638e
venkibric <venki.bathula@brickendon.com>
2022-04-08 12:37:56 +0100

    changed size of datatypes

--- tenders-api/update_v10.sql
+++ tenders-api/update_v10.sql
@@ -5,12 +5,12 @@ Tenders DB Update script: v10 (question_and_answer table)
 
 CREATE TABLE public.question_and_answer (
 	question_id SERIAL PRIMARY KEY NOT NULL,
-	question varchar(2000) NOT NULL,
-	answer varchar(2000) NOT NULL,
+	question varchar(500) NOT NULL,
+	answer varchar(500) NOT NULL,
 	event_id int4 NOT NULL,
-	created_by varchar(2000) NOT NULL,
+	created_by varchar(50) NOT NULL,
 	created_at timestamp NOT NULL,
-	updated_by varchar(2000) NULL,
+	updated_by varchar(50) NULL,
 	updated_at timestamp null
 );
 
