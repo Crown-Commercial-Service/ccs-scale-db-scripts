24f52fd9bfc1edebcf2fe74fb4b11f464eb47fc3
venkibric <venki.bathula@brickendon.com>
2022-07-14 11:28:27 +0100

    changed table name

--- tenders-api/update_v17.sql
+++ tenders-api/update_v17.sql
@@ -3,19 +3,19 @@
 Tenders DB Update script: v17 (Award details table)
 */
 
-CREATE TABLE award_details (
+CREATE TABLE contract_details (
 	contract_id SERIAL PRIMARY KEY NOT NULL,
-	award_id int4 NOT NULL,
+	award_id varchar(50) NOT NULL,
 	event_id int4 NOT NULL,
 	contract_status varchar(10) NOT NULL,
-	start_date timestamp NOT NULL,
-	end_date timestamp null,
 	created_by varchar(50) NOT NULL,
 	created_at timestamp NOT NULL,
 	updated_by varchar(50) NULL,
-	updated_at timestamp NULL
+	updated_at timestamp NULL,
+	CONSTRAINT award_details_pkey PRIMARY KEY (contract_id)
 );
 
-ALTER TABLE award_details
-ADD CONSTRAINT procurement_event_fk FOREIGN KEY (event_id)
-    REFERENCES procurement_events(event_id); 
+-- contract_details foreign keys
+ALTER TABLE contract_details ADD CONSTRAINT procurement_event_fk FOREIGN KEY (event_id) REFERENCES procurement_events(event_id);
+    
+    
