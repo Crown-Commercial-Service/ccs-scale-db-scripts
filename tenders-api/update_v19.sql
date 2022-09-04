b4de3ff582e490aaf272739f6eb07ecf5f21366b
venkibric <venki.bathula@brickendon.com>
2022-09-05 00:29:31 +0530

    add column

--- tenders-api/update_v19.sql
+++ tenders-api/update_v19.sql
@@ -5,3 +5,7 @@ Tenders DB Update script: v19 - Added extra columns for categorize different agr
 ALTER TABLE public.document_templates ADD commercial_agreement_number varchar(20);
 ALTER TABLE public.document_templates ADD lot_number varchar(20);
 ALTER TABLE public.document_templates ADD template_group int4;
+
+ALTER TABLE public.document_template_sources ADD CONSTRAINT check_source_type CHECK (((source_type)::text = ANY (ARRAY[('JSON'::character varying)::text, ('JAVA'::character varying)::text, ('STATIC'::character varying)::text, ('SQL'::character varying)::text])));
+
+ALTER TABLE public.document_template_sources ADD conditional_value varchar(2048) NULL;
