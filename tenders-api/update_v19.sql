3d624b213b42d11766711c1ee78d67c895166baa
venkibric <venki.bathula@brickendon.com>
2022-09-06 12:56:15 +0530

    removed public schema

--- tenders-api/update_v19.sql
+++ tenders-api/update_v19.sql
@@ -2,10 +2,10 @@
 Tenders DB Update script: v19 - Added extra columns for categorize different agreement templates                                                                                  answer
 */
 
-ALTER TABLE public.document_templates ADD commercial_agreement_number varchar(20);
-ALTER TABLE public.document_templates ADD lot_number varchar(20);
-ALTER TABLE public.document_templates ADD template_group int4;
+ALTER TABLE document_templates ADD commercial_agreement_number varchar(20);
+ALTER TABLE document_templates ADD lot_number varchar(20);
+ALTER TABLE document_templates ADD template_group int4;
 
-ALTER TABLE public.document_template_sources ADD CONSTRAINT check_source_type CHECK (((source_type)::text = ANY (ARRAY[('JSON'::character varying)::text, ('JAVA'::character varying)::text, ('STATIC'::character varying)::text, ('SQL'::character varying)::text])));
+ALTER TABLE document_template_sources ADD CONSTRAINT check_source_type CHECK (((source_type)::text = ANY (ARRAY[('JSON'::character varying)::text, ('JAVA'::character varying)::text, ('STATIC'::character varying)::text, ('SQL'::character varying)::text])));
 
-ALTER TABLE public.document_template_sources ADD conditional_value varchar(2048) NULL;
+ALTER TABLE document_template_sources ADD conditional_value varchar(2048) NULL;
