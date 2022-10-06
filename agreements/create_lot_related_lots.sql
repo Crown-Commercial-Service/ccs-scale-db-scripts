096094824408e7926ffdc1748be1fb9c3d9687dd
NToppingCCS <nathan.topping@crowncommercial.gov.uk>
2022-10-06 09:28:58 +0100

    Removing all the unneeded data population scripts (they're moving to the other repo)

--- agreements/create_lot_related_lots.sql
+++
@@ -1,8 +0,0 @@
-/*
-Title       create_lot_related_lots.sql
-Date        June 23rd 2020
-Author      Trevor Cummings
-Description This file is a script to create data for lot related lots        
-*/
-INSERT INTO LOT_RELATED_LOTS (lot_id, lot_rule_id, relationship_description)
-VALUES (10,6,'FurtherCompetitionWhenBudgetExceeded');
