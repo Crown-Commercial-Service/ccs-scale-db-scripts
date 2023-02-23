0b8b72b77dd566382de9b3f0dba95b0495836302
Chee-Ng <58430495+Chee-Ng@users.noreply.github.com>
2023-02-23 17:04:59 +0000

    duplicated scripts, moved to create

--- agreements-service/patches/20221128_7320.sql
+++
@@ -1,6 +0,0 @@
-/** Unique constraints added for Organisations and trading organisations to avoid duplicate entites 
-Before running this script, existing duplicates in the relevant tables must be cleanedup.
-*/
-
-ALTER TABLE organisations ADD CONSTRAINT organisations_entitycode_uq UNIQUE (entity_id,registry_code);
-ALTER TABLE trading_organisations ADD CONSTRAINT org_trading_name_uq UNIQUE (organisation_id,trading_organisation_name);
