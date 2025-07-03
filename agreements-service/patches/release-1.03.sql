219b047776c534bf269324e6624fe2975d03ef98
Raja Reddy <raja.dodda@kortext.com>
2025-07-03 09:20:33 +0100

    script to clean up dummy entity id

--- agreements-service/patches/release-1.03.sql
+++ agreements-service/patches/release-1.03.sql
@@ -5,16 +5,15 @@ Author      Raja D
 Description clean up duplicate data and added constraint to to make organisation entity_id NOT NULL
 */
 
-WITH duplicate_entity_ids AS (
-    SELECT entity_id
-    FROM organisations
-    WHERE entity_id IS NOT NULL
-    GROUP BY entity_id
-    HAVING COUNT(*) > 1
-)
 DELETE FROM organisations
 WHERE entity_id IS NULL
-   OR entity_id IN (SELECT entity_id FROM duplicate_entity_ids);
+   OR entity_id IN (
+       SELECT entity_id
+       FROM organisations
+       WHERE entity_id IS NOT NULL
+       GROUP BY entity_id
+       HAVING COUNT(*) > 1
+   );
 
 ALTER TABLE organisations
 ALTER COLUMN entity_id SET NOT NULL;
